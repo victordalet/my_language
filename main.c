61801c16e2139eea4bf720308884b043cecad8b8
victordalet <victordalet@protonmail.com>
2024-10-03 21:25:38 +0200

    feat(array): Add detection of int arrayu

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[])
 	int count_number = 0;
 	int count_strings = 0;
 	int count_floats = 0;
+	int count_number_int_array = 0;
 
 	char **numbers_name = malloc(count_number * sizeof(char *));
 	int *number_value = malloc(count_number * sizeof(int));
@@ -29,18 +30,80 @@ int main(int argc, char *argv[])
 	char **strings_name = malloc(count_strings * sizeof(char *));
 	char **strings_value = malloc(count_strings * sizeof(char *));
 
+	char **int_array_name = malloc(count_number_int_array * sizeof(char *));
+	int **int_array_value = malloc(count_number_int_array * sizeof(int *));
+	int *int_array_size_value =
+	    malloc(count_number_int_array * sizeof(int));
+
 	for (int i = 0; i < number_line; i++) {
 		char *line = read_file(argv[1], i);
 
 		if (!is_comment(line)) {
 			if (is_var(line)) {
 				if (is_string(line)) {
+					strings_name =
+					    realloc(strings_name,
+						    sizeof(char *) *
+						    count_strings + 1);
+					strings_value =
+					    realloc(strings_value,
+						    sizeof(char *) *
+						    count_strings + 1);
 					add_string(strings_name, strings_value,
 						   &count_strings, line);
+				} else if (is_tab_of_int(line)) {
+
+					int_array_name =
+					    realloc(int_array_name,
+						    sizeof(char *) *
+						    count_number_int_array + 1);
+					int_array_value =
+					    realloc(int_array_value,
+						    sizeof(int *) *
+						    count_number_int_array + 1);
+					int_array_size_value =
+					    realloc(int_array_size_value,
+						    sizeof(int) *
+						    count_number_int_array + 1);
+					add_int_array(int_array_name,
+						      int_array_value,
+						      int_array_size_value,
+						      &count_number_int_array,
+						      line);
+
+					for (int a = 0;
+					     a < count_number_int_array; a++) {
+						for (int b = 0;
+						     b <
+						     int_array_size_value[a];
+						     b++) {
+							printf("%d ",
+							       int_array_value
+							       [a][b]);
+						}
+						printf("\n");
+					}
+
 				} else if (is_float(line)) {
+					floats_name =
+					    realloc(floats_name,
+						    sizeof(char *) *
+						    count_floats + 1);
+					float_value =
+					    realloc(float_value,
+						    sizeof(float) *
+						    count_floats + 1);
 					add_float(floats_name, float_value,
 						  &count_floats, line);
 				} else if (is_int(line)) {
+					numbers_name =
+					    realloc(numbers_name,
+						    sizeof(char *) *
+						    count_number + 1);
+					number_value =
+					    realloc(number_value,
+						    sizeof(int) * count_number +
+						    1);
 					add_int(numbers_name, number_value,
 						&count_number, line);
 				}
@@ -75,10 +138,14 @@ int main(int argc, char *argv[])
 	}
 	for (int i = 0; i < count_strings; i++) {
 		free(strings_name[i]);
+		free(strings_value[i]);
 	}
-
 	for (int i = 0; i < count_floats; i++) {
 		free(floats_name[i]);
+	}
+	for (int i = 0; i < count_number_int_array; i++) {
+		free(int_array_name[i]);
+		free(int_array_value[i]);
 	}
 	free(numbers_name);
 	free(number_value);
@@ -86,6 +153,9 @@ int main(int argc, char *argv[])
 	free(float_value);
 	free(strings_name);
 	free(strings_value);
+	free(int_array_name);
+	free(int_array_value);
+	free(int_array_size_value);
 
 	printf("\n");
 
--- src/var.c
+++ src/var.c
@@ -1,253 +1,367 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-void add_int(char **name, int *values, int *count, char *line)
-{
-	int i = 0;
+void add_int(char **name, int *values, int *count, char *line) {
+    int i = 0;
 
-	int count_new_name = 0;
+    int count_new_name = 0;
 
-	int mid = 0;
+    int mid = 0;
 
-	int count_number_in_number = 0;
-	int multiply = 1;
-	int number = 0;
+    int count_number_in_number = 0;
+    int multiply = 1;
+    int number = 0;
 
-	while (line[i] != '\0' && line[i] != '=') {
-		if (line[i] != ' ')
-			count_new_name++;
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ')
+            count_new_name++;
 
-		i++;
-	}
+        i++;
+    }
 
-	i = 0;
-
-    /*--------------------------------------------------------------*/
-
-	char *var_name = malloc(sizeof(char) * count_new_name + 1);
-
-	count_new_name = 0;
-
-	while (line[i] != '\0' && line[i] != '=') {
-		if (line[i] != ' ') {
-			var_name[count_new_name] = line[i];
-			count_new_name++;
-		}
-
-		i++;
-	}
-
-	var_name[count_new_name] = '\0';
-
-	name[*count] = var_name;
+    i = 0;
 
     /*--------------------------------------------------------------*/
 
-	while (line[i] != '\0') {
-		if (line[i] >= 48 && line[i] <= 57) {
-			count_number_in_number++;
-			multiply *= 10;
-		}
+    char *var_name = malloc(sizeof(char) * count_new_name + 1);
 
-		i++;
-	}
+    count_new_name = 0;
 
-	multiply /= 10;
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ') {
+            var_name[count_new_name] = line[i];
+            count_new_name++;
+        }
 
-	while (line[mid] != '\0') {
-		if (line[mid] >= 48 && line[mid] <= 57) {
-			number += (line[mid] - 48) * multiply;
-			multiply /= 10;
-		}
+        i++;
+    }
 
-		mid++;
-	}
+    var_name[count_new_name] = '\0';
 
-	values[*count] = number;
+    name[*count] = var_name;
 
-	*count += 1;
+    /*--------------------------------------------------------------*/
+
+    while (line[i] != '\0') {
+        if (line[i] >= 48 && line[i] <= 57) {
+            count_number_in_number++;
+            multiply *= 10;
+        }
+
+        i++;
+    }
+
+    multiply /= 10;
+
+    while (line[mid] != '\0') {
+        if (line[mid] >= 48 && line[mid] <= 57) {
+            number += (line[mid] - 48) * multiply;
+            multiply /= 10;
+        }
+
+        mid++;
+    }
+
+    values[*count] = number;
+
+    *count += 1;
 };
 
-void add_float(char **name, float *values, int *count, char *line)
-{
-	int i = 0;
+void add_float(char **name, float *values, int *count, char *line) {
+    int i = 0;
 
-	int count_new_name = 0;
-	int new_value = 0;
+    int count_new_name = 0;
+    int new_value = 0;
 
-	int mid = 0;
+    int mid = 0;
 
-	int count_number_in_number = 0;
-	int multiply = 1;
-	float number = 0;
+    int count_number_in_number = 0;
+    int multiply = 1;
+    float number = 0;
 
-	int decimal = 0;
-	int multiply_decimal = 1;
+    int decimal = 0;
+    int multiply_decimal = 1;
 
-	while (line[i] != '\0' && line[i] != '=') {
-		if (line[i] != ' ')
-			count_new_name++;
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ')
+            count_new_name++;
 
-		i++;
-	}
+        i++;
+    }
 
-	i = 0;
-
-    /*--------------------------------------------------------------*/
-
-	char *var_name = malloc(sizeof(char) * count_new_name + 1);
-
-	count_new_name = 0;
-
-	while (line[i] != '\0' && line[i] != '=') {
-		if (line[i] != ' ') {
-			var_name[count_new_name] = line[i];
-			count_new_name++;
-		}
-
-		i++;
-	}
-
-	var_name[count_new_name] = '\0';
-
-	name[*count] = var_name;
+    i = 0;
 
     /*--------------------------------------------------------------*/
 
-	while (line[i] != '\0' && line[i] != '.') {
-		if (line[i] >= 48 && line[i] <= 57) {
-			count_number_in_number++;
-			multiply *= 10;
-		}
+    char *var_name = malloc(sizeof(char) * count_new_name + 1);
 
-		i++;
-	}
+    count_new_name = 0;
 
-	multiply /= 10;
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ') {
+            var_name[count_new_name] = line[i];
+            count_new_name++;
+        }
 
-	while (line[mid] != '\0' && line[mid] != '.') {
-		if (line[mid] >= 48 && line[mid] <= 57) {
-			number += (line[mid] - 48) * multiply;
-			multiply /= 10;
-		}
+        i++;
+    }
 
-		mid++;
-	}
+    var_name[count_new_name] = '\0';
 
-    /*--------------------------------------------------------------*/
-	count_number_in_number = 0;
-	multiply = 1;
-	i++;
-	mid++;
-
-	while (line[i] != '\0') {
-		if (line[i] >= 48 && line[i] <= 57) {
-			count_number_in_number++;
-			multiply *= 10;
-		}
-
-		i++;
-	}
-
-	multiply /= 10;
-
-	while (line[mid] != '\0' && line[mid] != '.') {
-		if (line[mid] >= 48 && line[mid] <= 57) {
-			decimal += (line[mid] - 48) * multiply;
-			multiply /= 10;
-		}
-
-		mid++;
-	}
+    name[*count] = var_name;
 
     /*--------------------------------------------------------------*/
 
-	for (i = 0; i < count_number_in_number; i++) {
-		multiply_decimal *= 10;
-	}
+    while (line[i] != '\0' && line[i] != '.') {
+        if (line[i] >= 48 && line[i] <= 57) {
+            count_number_in_number++;
+            multiply *= 10;
+        }
 
-	values[*count] = (number + decimal) / multiply_decimal;
+        i++;
+    }
 
-	*count += 1;
+    multiply /= 10;
+
+    while (line[mid] != '\0' && line[mid] != '.') {
+        if (line[mid] >= 48 && line[mid] <= 57) {
+            number += (line[mid] - 48) * multiply;
+            multiply /= 10;
+        }
+
+        mid++;
+    }
+
+    /*--------------------------------------------------------------*/
+    count_number_in_number = 0;
+    multiply = 1;
+    i++;
+    mid++;
+
+    while (line[i] != '\0') {
+        if (line[i] >= 48 && line[i] <= 57) {
+            count_number_in_number++;
+            multiply *= 10;
+        }
+
+        i++;
+    }
+
+    multiply /= 10;
+
+    while (line[mid] != '\0' && line[mid] != '.') {
+        if (line[mid] >= 48 && line[mid] <= 57) {
+            decimal += (line[mid] - 48) * multiply;
+            multiply /= 10;
+        }
+
+        mid++;
+    }
+
+    /*--------------------------------------------------------------*/
+
+    for (i = 0; i < count_number_in_number; i++) {
+        multiply_decimal *= 10;
+    }
+
+    values[*count] = (number + decimal) / multiply_decimal;
+
+    *count += 1;
 };
 
-void add_string(char **name, char **values, int *count, char *line)
-{
-	int i = 0;
+void add_string(char **name, char **values, int *count, char *line) {
+    int i = 0;
 
-	int count_new_name = 0;
+    int count_new_name = 0;
 
-	int mid = 0;
+    int mid = 0;
 
-	int number_char = 0;
+    int number_char = 0;
 
-	int nb_quote = 0;
+    int nb_quote = 0;
 
-	while (line[i] != '\0' && line[i] != '=') {
-		if (line[i] != ' ')
-			count_new_name++;
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ')
+            count_new_name++;
 
-		i++;
-	}
+        i++;
+    }
 
-	i = 0;
-
-    /*--------------------------------------------------------------*/
-
-	char *var_name = malloc(sizeof(char) * count_new_name + 1);
-
-	count_new_name = 0;
-
-	while (line[i] != '\0' && line[i] != '=') {
-		if (line[i] != ' ') {
-			var_name[count_new_name] = line[i];
-			count_new_name++;
-		}
-
-		i++;
-	}
-
-	var_name[count_new_name] = '\0';
-
-	name[*count] = var_name;
-
-	mid = i;
+    i = 0;
 
     /*--------------------------------------------------------------*/
 
-	while (line[i] != '\0') {
-		if (line[i] == '"')
-			if (!nb_quote)
-				nb_quote++;
-			else
-				break;
+    char *var_name = malloc(sizeof(char) * count_new_name + 1);
 
-		if (nb_quote) {
-			number_char++;
-		}
-		i++;
-	}
+    count_new_name = 0;
 
-	nb_quote = 0;
-	char *new_value = malloc(sizeof(char) * number_char + 1);
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ') {
+            var_name[count_new_name] = line[i];
+            count_new_name++;
+        }
 
-	for (i = 1; i < number_char; i++) {
-		if (line[mid + i] == '"')
-			if (!nb_quote)
-				nb_quote++;
-			else
-				break;
+        i++;
+    }
 
-		if (!nb_quote) {
-			mid++;
-			i -= 1;
-		} else {
-			new_value[i - 1] = line[mid + i + 1];
-		}
-	}
+    var_name[count_new_name] = '\0';
 
-	new_value[number_char - 1] = '\0';
+    name[*count] = var_name;
 
-	values[*count] = new_value;
+    mid = i;
 
-	*count += 1;
+    /*--------------------------------------------------------------*/
+
+    while (line[i] != '\0') {
+        if (line[i] == '"')
+            if (!nb_quote)
+                nb_quote++;
+            else
+                break;
+
+        if (nb_quote) {
+            number_char++;
+        }
+        i++;
+    }
+
+    nb_quote = 0;
+    char *new_value = malloc(sizeof(char) * number_char + 1);
+
+    for (i = 1; i < number_char; i++) {
+        if (line[mid + i] == '"')
+            if (!nb_quote)
+                nb_quote++;
+            else
+                break;
+
+        if (!nb_quote) {
+            mid++;
+            i -= 1;
+        } else {
+            new_value[i - 1] = line[mid + i + 1];
+        }
+    }
+
+    new_value[number_char - 1] = '\0';
+
+    values[*count] = new_value;
+
+    *count += 1;
+};
+
+
+void add_int_array(char **name, int **values, int* nb_values,int *count, char *line) {
+
+    int i = 0;
+
+    int count_new_name = 0;
+
+    int mid = 0;
+
+    int count_number_in_number = 0;
+    int multiply = 1;
+    int number = 0;
+
+    int count_number = 0;
+
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ')
+            count_new_name++;
+
+        i++;
+    }
+
+    i = 0;
+
+    /*--------------------------------------------------------------*/
+
+    char *var_name = malloc(sizeof(char) * count_new_name + 1);
+
+    count_new_name = 0;
+
+    while (line[i] != '\0' && line[i] != '=') {
+        if (line[i] != ' ') {
+            var_name[count_new_name] = line[i];
+            count_new_name++;
+        }
+
+        i++;
+    }
+
+    var_name[count_new_name] = '\0';
+
+    name[*count] = var_name;
+
+
+
+    /*--------------------------------------------------------------*/
+    mid = i;
+
+    while (line[i] != '\0') {
+        if (line[i] == ',')
+            count_number++;
+
+        i++;
+    }
+
+
+    count_number++;
+    int *new_array = malloc(sizeof(int) * count_number);
+
+
+
+    /*--------------------------------------------------------------*/
+
+
+    for (int index = 0 ; index < count_number ; index++) {
+
+
+        number = 0;
+        multiply = 1;
+        count_number_in_number = 1;
+
+        while (line[i] != '\0') {
+            if (line[i] == ',' || line[i] == ']')
+                break;
+
+            if (line[i] >= 48 && line[i] <= 57) {
+                count_number_in_number++;
+                multiply *= 10;
+            }
+
+            i++;
+        }
+
+
+
+        while (line[mid] != '\0') {
+
+
+            if (line[mid] == ',' || line[mid] == ']')
+                break;
+
+            if (line[mid] >= 48 && line[mid] <= 57) {
+                number += (line[mid] - 48) * multiply;
+                multiply /= 10;
+            }
+
+            mid++;
+        }
+
+        i++;
+        mid++;
+        new_array[index] = number;
+
+
+
+    }
+
+
+
+    values[*count] = new_array;
+
+    nb_values[*count] = count_number;
+
+    *count += 1;
 };
